e8c3a4eb952de29ef831cf0a9ede859593628104
uchaen <u_chaen@naver.com>
2022-12-03 01:14:06 +0900

    Day12 dropdown-menu sliding

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,8 +7,23 @@ class Header extends Component {
         super(props);
         this.state = {
             access_Token: localStorage.getItem("accessToken"),
-            user_name: localStorage.getItem("user_name")
+            user_name: localStorage.getItem("user_name"),
+            isOn: false,
+            goDown: false
         }
+    }
+    dropdownMenuOn = () => {
+        this.setState({
+            isOn: true,
+            goDown: true
+        });
+    }
+    dropdownMenuOff = () => {
+        this.setState({ goDown: false }); //마우스 떼고 1초 후 슬라이드 업
+        setTimeout(() =>
+            this.setState({ isOn: false }),
+            1300
+        ); //1초 + 0.3초(애니메이션시간) 후 display:none
     }
     logout = (e) => {
         e.preventDefault();
@@ -20,36 +35,53 @@ class Header extends Component {
     }
     render() {
         return (
-            <header>
-                <div id="left">
-                    <Link to="/"><img style={{ height: "22px" }} src="assets/uroket-logo.jpg" alt="로고" /></Link>
-                    <div id="searchbar">
-                        <input type="text" id="searchinput" placeholder="해외상품을 검색해보세요!" />
-                        <img id="searchbtn" src="https://croket.s3.ap-northeast-2.amazonaws.com/icon/layout/search+1.png" alt="검색" />
+            <div className='Header'>
+                <Dropdown isOn={this.state.isOn} goDown={this.state.goDown} />
+                <header>
+                    <div id="left">
+                        <Link to="/"><img style={{ height: "22px" }} src="assets/uroket-logo.jpg" alt="로고" /></Link>
+                        <div id="searchbar">
+                            <input type="text" id="searchinput" placeholder="해외상품을 검색해보세요!" />
+                            <img id="searchbtn" src="https://croket.s3.ap-northeast-2.amazonaws.com/icon/layout/search+1.png" alt="검색" />
+                        </div>
                     </div>
-                </div>
 
-                <div id="middle">
-                    <div className="bigUnderbar">전세계상품 <Dropdown /> </div>
-                    <div> ㅣ </div>
-                    <div className="bigUnderbar">셀러이신가요? <Dropdown /> </div>
-                    <div> ㅣ </div>
-                    <div className="bigUnderbar">이벤트 <Dropdown /> </div>
-                </div>
+                    <div id="middle">
+                        <div className="bigUnderbar" onMouseOver={this.dropdownMenuOn} onMouseOut={this.dropdownMenuOff}>
+                            전세계상품
+                        </div>
+                        <div> ㅣ </div>
+                        <div className="bigUnderbar" onMouseOver={this.dropdownMenuOn} onMouseOut={this.dropdownMenuOff}>
+                            셀러이신가요?
+                        </div>
+                        <div> ㅣ </div>
+                        <div className="bigUnderbar" onMouseOver={this.dropdownMenuOn} onMouseOut={this.dropdownMenuOff}>
+                            이벤트
+                        </div>
+                    </div>
 
-                {this.state.access_Token ? //로그인 상태에 따라 변경
-                    <div id="right">
-                        <img className="miniUnderbar" src="https://croket.s3.ap-northeast-2.amazonaws.com/icon/ChatCenteredText.png" alt="메세지" />
-                        <img className="miniUnderbar" src="https://croket.s3.ap-northeast-2.amazonaws.com/icon/basket.png" alt="장바구니" />
-                        <img className="miniUnderbar" onClick={this.logout} src="assets/uroket-favicon.png" alt="프사" />
-                    </div>
-                    :
-                    <div>
-                        <Link to="/login" className="bigUnderbar" style={{ color: "blueviolet" }}>로그인</Link>
-                        <Link to="/ready_to_signup" className="bigUnderbar" style={{ color: "blueviolet" }}>회원가입</Link>
-                    </div>
-                }
-            </header>
+                    {this.state.access_Token ? //로그인 상태에 따라 변경
+                        <div id="right">
+                            <img className="miniUnderbar" src="https://croket.s3.ap-northeast-2.amazonaws.com/icon/ChatCenteredText.png" alt="메세지" />
+                            <img className="miniUnderbar" src="https://croket.s3.ap-northeast-2.amazonaws.com/icon/basket.png" alt="장바구니" />
+                            <div className="profile">
+                                <img className="miniUnderbar" src="assets/uroket-favicon.png" alt="프사" />
+                                <div className='profile-menu'>
+                                    <div>{this.state.user_name} 님</div>
+                                    <div>구매내역</div>
+                                    <div>셀러 활동 관리</div>
+                                    <div onClick={this.logout} >로그아웃</div>
+                                </div>
+                            </div>
+                        </div>
+                        :
+                        <div>
+                            <Link to="/login" className="bigUnderbar" style={{ color: "blueviolet" }}>로그인</Link>
+                            <Link to="/ready_to_signup" className="bigUnderbar" style={{ color: "blueviolet" }}>회원가입</Link>
+                        </div>
+                    }
+                </header>
+            </div>
         );
     }
 }
@@ -57,8 +89,9 @@ class Header extends Component {
 class Dropdown extends Component {
     render() {
         return (
-            <div className="dropdown-menu slide_down">
-                
+            //평소 : dd-m / mouseover : dd-m s-d / mouseout : dd-m s-u 후 평소
+            <div className={"dropdown-menu " + (this.props.isOn ? (this.props.goDown ? "slide-down" : "slide-up") : " ")} >
+
                 <div className="dropdown-content">
                     <div className='dropdown-title'>전세계상품</div>
                     <li>판매 상품 전체보기</li>
@@ -89,6 +122,5 @@ class Dropdown extends Component {
         );
     }
 }
-
 
 export default Header;
